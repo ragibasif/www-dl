839816989c1925505689668d2ca91a035be29c7f
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-05-12 19:18:50 -0400

    refactor and implement creating a subdirectory function

--- main.go
+++ main.go
@@ -3,48 +3,112 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"net/url"
 	"os"
 )
 
-// MVP Algorithm
+const separator string = "========================================"
+const mainDir string = "www-downloads"
+const defaultURL string = "https://httpbin.org/"
 
-// get URL,
+// TODO: add logging that can be turned on and off adhoc by the user
+// TODO: user should be able to run `Chrome /www-downloads/www.something.com`
+// where `www.something.com` was one of URLs, and browse the page offline
 
-// check with DNS (or local resolver) the IP address of the webserver,
-// connect to the IP,
-// and HTTP Headers and
-// HTTP request,
+// TODO: (MAYBE!) Add a config file that the user can use to override defaults...
 
-// get HTTP Response,
+// https://pkg.go.dev/os@go1.24.3#MkdirAll
+func createDir(dir string) {
+	err := os.MkdirAll(dir, 0755)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+}
 
-// save to file
+func getUrlList(args []string) []string {
+	var u []string
+	if len(args) > 1 {
+		u = args[1:]
+	} else {
+		u = []string{defaultURL}
+	}
+	return u
+}
+
+// https://gobyexample.com/url-parsing
+func parseURL(s string) *url.URL {
+	u, err := url.Parse(s)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	return u
+}
+
+// generic slice printing util
+func printSlice[S ~[]E, E any](s S) {
+	fmt.Println(separator)
+	fmt.Printf("Slice: %v Type: %T\nLength = %d Capacity = %d\n", s, s, len(s), cap(s))
+	for i, v := range s {
+		fmt.Printf("%v : %v\n", i, v)
+	}
+	fmt.Println(separator)
+}
+
+var client *http.Client = &http.Client{}
+
+// https://pkg.go.dev/net/http@go1.24.3#Get
+func sendHttpRequests(s []*url.URL) {
+
+	for i, v := range s {
+		log.Println(i)
+
+		res, err := client.Get(v.String())
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer res.Body.Close()
+		body, err := io.ReadAll(res.Body)
+		if res.StatusCode > 299 {
+			log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+		} else {
+			log.Print(res.StatusCode)
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		// fmt.Printf("%s", body)
+		newDir := "www." + v.Hostname()
+		newPath := mainDir + "/" + newDir
+		createDir(newPath)
+		err = os.WriteFile(newPath+"/index.html", body, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	}
+}
+
+// https://go.dev/tour/moretypes/15
+func makeParsedUrlList(url_list []string) []*url.URL {
+	var s []*url.URL
+	for i := range url_list {
+		t := parseURL(url_list[i])
+		s = append(s, t)
+	}
+	return s
+}
 
 func main() {
 
-	var URL string
-	if len(os.Args) > 1 {
-		URL = os.Args[1]
-	} else {
-		URL = "https://go.dev/"
-	}
+	createDir(mainDir)
 
-	resp, err := http.Get(URL)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	fmt.Println("Response status:", resp.Status)
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	e := os.WriteFile("./index.html", body, 0644)
-	if e != nil {
-		panic(e)
-	}
+	url_list := getUrlList(os.Args)
+	url_list_parsed := makeParsedUrlList(url_list)
+	printSlice(url_list_parsed)
+	sendHttpRequests(url_list_parsed)
 
 }
